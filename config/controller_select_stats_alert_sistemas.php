c35cb9ad9d429b6ce1c05fc14275ffa606078b4f
Marcelo Cardenas <mcardenas@i-med.cl>
2017-11-29 01:22:52 -0300

    fix filter query solo sistemas

--- config/controller_select_stats_alert_sistemas.php
+++ config/controller_select_stats_alert_sistemas.php
@@ -11,19 +11,19 @@ $result_atrasados_hoy = $conn->query($sql_atrasados_hoy) or die('Query failed: '
 
 $sql_abiertos_hoy = "
 SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt
-WHERE tkt.status_id in(1,6,7,8) AND tkt.staff_id in(1,2,3,4,5) AND tkt.created >= CURDATE() && tkt.created < (CURDATE() + INTERVAL 1 DAY);";
+WHERE tkt.status_id in(1,6,7,8) AND tkt.created >= CURDATE() && tkt.created < (CURDATE() + INTERVAL 1 DAY);";
 
 $result_abiertos_hoy = $conn->query($sql_abiertos_hoy) or die('Query failed: ' . mysql_error());
 
 $sql_cerrados_hoy = "
 SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt
-WHERE tkt.status_id in(2,3,4) AND tkt.staff_id in(1,2,3,4,5) AND tkt.created >= CURDATE() && tkt.created < (CURDATE() + INTERVAL 1 DAY);";
+WHERE tkt.status_id in(2,3,4) AND tkt.created >= CURDATE() && tkt.created < (CURDATE() + INTERVAL 1 DAY);";
 
 $result_cerrados_hoy = $conn->query($sql_cerrados_hoy) or die('Query failed: ' . mysql_error());
 
 $sql_total_hoy = "
 SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt
-WHERE tkt.number like '%SIST%' AND tkt.created >= CURDATE() && tkt.created < (CURDATE() + INTERVAL 1 DAY);";
+WHERE tkt.created >= CURDATE() && tkt.created < (CURDATE() + INTERVAL 1 DAY);";
 
 $result_total_hoy = $conn->query($sql_total_hoy) or die('Query failed: ' . mysql_error());
 
@@ -37,19 +37,18 @@ $result_atrasados = $conn->query($sql_atrasados) or die('Query failed: ' . mysql
 
 $sql_abiertos = "
 SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt
-WHERE tkt.status_id in(1,6,7,8) AND tkt.staff_id in(1,2,3,4,5) ;";
+WHERE tkt.status_id in(1,6,7,8);";
 
 $result_abiertos = $conn->query($sql_abiertos) or die('Query failed: ' . mysql_error());
 
 $sql_cerrados = "
 SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt
-WHERE tkt.status_id in(2,3,4) AND tkt.staff_id in(1,2,3,4,5) ;";
+WHERE tkt.status_id in(2,3,4);";
 
 $result_cerrados = $conn->query($sql_cerrados) or die('Query failed: ' . mysql_error());
 
 $sql_total = "
-SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt 
-WHERE tkt.number like '%SIST%' ;";
+SELECT count(tkt.ticket_id) as ticket FROM osticket.ost_ticket tkt;";
 
 $result_total = $conn->query($sql_total) or die('Query failed: ' . mysql_error());
 
